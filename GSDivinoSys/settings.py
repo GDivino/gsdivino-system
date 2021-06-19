80f59a5469fd8b8a0a4c76778226a888541d6074
GDivino <gio.divino3649@gmail.com>
2021-06-19 12:47:28 +0800

    Change staticfiles_dir to app directory

--- GSDivinoSys/settings.py
+++ GSDivinoSys/settings.py
@@ -127,11 +127,12 @@ USE_TZ = True
 
 PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 
 STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
+    os.path.join(PROJECT_ROOT, 'GSDivinoApp', 'static'),
 )
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
