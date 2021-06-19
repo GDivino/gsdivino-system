1cb0d4a1385ff66ef5db1bbdf44669346fdec84f
GDivino <gio.divino3649@gmail.com>
2021-06-19 11:29:27 +0800

    Change staticfiles_storage

--- GSDivinoSys/settings.py
+++ GSDivinoSys/settings.py
@@ -134,7 +134,7 @@ STATICFILES_DIRS = (
     os.path.join(PROJECT_ROOT, 'static'),
 )
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 # STATIC_URL = '/static/'
 # STATIC_DIRS = os.path.join(BASE_DIR, 'static')
